06bccd64654f7eed2882cd5d68d3e114567f360e
JIAHONG\CheahHong <jiahong9705@gmail.com>
2017-09-28 23:43:29 +0800

    add splash screen

--- app/src/main/java/com/example/cheahhong/dbest/SplashScreen.java
+++ app/src/main/java/com/example/cheahhong/dbest/SplashScreen.java
@@ -3,7 +3,6 @@ package com.example.cheahhong.dbest;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PixelFormat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.animation.Animation;
