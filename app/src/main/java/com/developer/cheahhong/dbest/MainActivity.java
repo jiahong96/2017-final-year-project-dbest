629afc4d8dae417658f7550aab0657e542b2f405
JIAHONG\CheahHong <jiahong9705@gmail.com>
2017-10-30 00:18:01 +0800

    add inquiry remove function

--- app/src/main/java/com/developer/cheahhong/dbest/MainActivity.java
+++ app/src/main/java/com/developer/cheahhong/dbest/MainActivity.java
@@ -2,20 +2,25 @@ package com.developer.cheahhong.dbest;
 
 import android.app.Activity;
 import android.app.NotificationManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -95,6 +100,7 @@ public class MainActivity extends BaseActivity{
         queryRef.keepSynced(true);
         refNormalInquiry = database.getReference("inquiries");
         refAdInquiry = database.getReference("adinquiries");
+        refNormalInquiry.keepSynced(true);
         refAdInquiry.keepSynced(true);
         //Log.d("database Reference",queryRef.toString());
 
@@ -216,89 +222,97 @@ public class MainActivity extends BaseActivity{
             protected void populateViewHolder(final MessageViewHolder viewHolder, Inquiry model, final int position) {
                 msg = new ChatMessage();
                 viewHolder.notification.setVisibility(View.GONE);
+                RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) viewHolder.rl.getLayoutParams();
                 String previewText = "";
-                if(model.getLastMessage()!=null && model.getLastMessage().getMessageText()!=null){
-                    msg = model.getLastMessage();
-                    viewHolder.time.setText(DateFormat.format("MMM dd, HH:mm",
-                            (msg.getMessageTime()*-1)));
-                    viewHolder.time.setTextColor(Color.parseColor("#808080"));
-                    if(model.getMsgUnreadCountForMobile()>0) {
-                        //Log.d("count", String.valueOf(model.getMsgUnreadCountForMobile()));
-                        viewHolder.notification.setVisibility(View.VISIBLE);
-                        viewHolder.notification.setText(String.valueOf(model.getMsgUnreadCountForMobile()));
-                        viewHolder.time.setTextColor(Color.parseColor("#40C4FF"));
-                    }
 
-                    previewText = msg.getMessageText();
-                    //Log.d("containN",previewText);
-                    if(previewText.contains("\n")){
+                final float scale = MainActivity.this.getResources().getDisplayMetrics().density;
+                int horizontal = (int) (16 * scale + 0.5f);
+                int top = (int) (10 * scale + 0.5f);
+                
+                if(model.getUserStatus().equalsIgnoreCase("active")){
+                    layoutParams.setMargins(horizontal,top,horizontal,0);
+                    viewHolder.rl.setLayoutParams(layoutParams);
+
+                    viewHolder.card.setVisibility(View.VISIBLE);
+                    viewHolder.rl.setVisibility(View.VISIBLE);
+                    viewHolder.rl2.setVisibility(View.VISIBLE);
+                    viewHolder.inquiryTxt.setVisibility(View.VISIBLE);
+                    viewHolder.content.setVisibility(View.VISIBLE);
+                    viewHolder.time.setVisibility(View.VISIBLE);
+
+                    if(model.getLastMessage()!=null && model.getLastMessage().getMessageText()!=null){
+                        msg = model.getLastMessage();
+                        viewHolder.time.setText(DateFormat.format("MMM dd, HH:mm",
+                                (msg.getMessageTime()*-1)));
+                        viewHolder.time.setTextColor(Color.parseColor("#808080"));
+                        if(model.getMsgUnreadCountForMobile()>0) {
+                            //Log.d("count", String.valueOf(model.getMsgUnreadCountForMobile()));
+                            viewHolder.notification.setVisibility(View.VISIBLE);
+                            viewHolder.notification.setText(String.valueOf(model.getMsgUnreadCountForMobile()));
+                            viewHolder.time.setTextColor(Color.parseColor("#40C4FF"));
+                        }
+
+                        previewText = msg.getMessageText();
                         //Log.d("containN",previewText);
-                        previewText = previewText.replaceAll("\n"," ");
-                        //Log.d("removeN",previewText);
-                    }
+                        if(previewText.contains("\n")){
+                            //Log.d("containN",previewText);
+                            previewText = previewText.replaceAll("\n"," ");
+                            //Log.d("removeN",previewText);
+                        }
 
-                    if(previewText.equals("Image")){
-                        previewText = "[Image attachment]";
-                    }
+                        if(previewText.equals("Image")){
+                            previewText = "[Image attachment]";
+                        }
 
-                    if(previewText.length()>38){
-                        viewHolder.content.setText(previewText.substring(0,35)+"...");
+                        if(previewText.length()>38){
+                            viewHolder.content.setText(previewText.substring(0,35)+"...");
+                        }else{
+                            viewHolder.content.setText(previewText);
+                        }
                     }else{
-                        viewHolder.content.setText(previewText);
+                        viewHolder.time.setText("");
+                        viewHolder.content.setText("");
                     }
+
+                    viewHolder.inquiryTxt.setText(model.getInquiryName());
+                    viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //Log.d("OnClick", "You clicked on "+ inquiryName);
+                            startMessageActivity(viewHolder.getAdapterPosition());
+                        }
+                    });
+
+                    viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            //Log.d("OnClick", "You Long clicked on "+ inquiryName);
+                            //startMessageActivity(viewHolder.getAdapterPosition());
+                            return false;
+                        }
+                    });
+
+                    viewHolder.itemView.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            //Log.d("OnClick", "You touch on "+ inquiryName);
+                            return false;
+                        }
+                    });
                 }else{
-                    viewHolder.time.setText("");
-                    viewHolder.content.setText("");
+                    layoutParams.setMargins(0, 0, 0, 0);
+                    viewHolder.rl.setLayoutParams(layoutParams);
+
+                    viewHolder.card.setVisibility(View.GONE);
+                    viewHolder.rl.setVisibility(View.GONE);
+                    viewHolder.rl2.setVisibility(View.GONE);
+                    viewHolder.inquiryTxt.setVisibility(View.GONE);
+                    viewHolder.content.setVisibility(View.GONE);
+                    viewHolder.time.setVisibility(View.GONE);
                 }
-
-//                int countBreak = 0;
-//                File imgFile;
-//                for(int i=0;i<model.getItems().size();i++){
-//                    //Log.d("imgpath","count");
-//                    imgFile = new File(model.getItems().get(i).getImageFileUrl());
-//                    //Log.d("imgpath",imgFile.toString());
-//                    if(imgFile.exists()){
-//                        //Log.d("imgpath","exist");
-//                        countBreak++;
-//                        try {
-//                            viewHolder.img.setImageBitmap(handleSamplingAndRotationBitmap(getApplicationContext(),Uri.parse(model.getItems().get(0).getImageFileUri()),model.getItems().get(0).getImageFileUrl()));
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    if(countBreak>0){
-//                        break;
-//                    }
-//                }
-
-                viewHolder.inquiryTxt.setText(model.getInquiryName());
-                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //Log.d("OnClick", "You clicked on "+ inquiryName);
-                        startMessageActivity(viewHolder.getAdapterPosition());
-                    }
-                });
-
-                viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-                        //Log.d("OnClick", "You Long clicked on "+ inquiryName);
-                        startMessageActivity(viewHolder.getAdapterPosition());
-                        return false;
-                    }
-                });
-
-                viewHolder.itemView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        //Log.d("OnClick", "You touch on "+ inquiryName);
-                        return false;
-                    }
-                });
             }
         };
+
         mRecyclerView.setAdapter(adapter);
         adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
@@ -318,16 +332,32 @@ public class MainActivity extends BaseActivity{
 
 
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(final MenuItem item) {
         //get selected menu item (delete, edit etc)
-        int menuItemIndex = item.getItemId();
-        String menuItemName = menuItems[menuItemIndex];
-        if(menuItemName.equals("Delete")){
+        //int menuItemIndex = item.getItemId();
+        String menuItemName = menuItems[0];
+        if(menuItemName.equals("Remove")){
             //Log.d("Delete", "You choose to delete position"+item.getGroupId());
             //Log.d("Delete", "You choose to delete "+adapter.getItem(item.getGroupId()).getInquiryName());
-            adapter.getRef(item.getGroupId()).removeValue();
-            // database.getReference("conversations/"+adapter.getItem(item.getGroupId()).getInquiryID()).removeValue();
-            database.getReference("adconversations/"+user.getUid()+"/"+adapter.getItem(item.getGroupId()).getInquiryID()+"?"+adapter.getItem(item.getGroupId()).getInquiryName()).removeValue();
+            //adapter.getRef(item.getItemId()).removeValue();
+            final String inqID = adapter.getItem(item.getItemId()).getInquiryID();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to remove this inquiry?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            refNormalInquiry.child(inqID).child("userStatus").setValue("inactive");
+                            refAdInquiry.child(user.getUid()).child(inqID).child("userStatus").setValue("inactive");
+                            adapter.notifyItemRemoved(item.getItemId());
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
         }
         return true;
     }
@@ -406,24 +436,27 @@ public class MainActivity extends BaseActivity{
 
     public static class MessageViewHolder extends RecyclerView.ViewHolder{
         TextView inquiryTxt,content,time,notification;
-        ImageView img;
+        CardView card;
+        RelativeLayout rl,rl2;
         public MessageViewHolder(View itemView) {
             super(itemView);
-//            img = (ImageView)itemView.findViewById(R.id.imageView1);
+            card = (CardView)itemView.findViewById(R.id.CardView);
+            rl = (RelativeLayout)itemView.findViewById(R.id.rl);
+            rl2 = (RelativeLayout)itemView.findViewById(R.id.relativeContent);
             inquiryTxt = (TextView)itemView.findViewById(R.id.title);
             content = (TextView)itemView.findViewById(R.id.content);
             time = (TextView)itemView.findViewById(R.id.time);
             notification = (TextView)itemView.findViewById(R.id.notification);
-//            itemView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
-//                @Override
-//                public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
-//                    //Log.d("Position", "You long clicked on position "+getAdapterPosition());
-//                    Log.w("OnLongClick", "You long clicked on "+adapter.getItem(getAdapterPosition()).getInquiryName());
-//                    for (int i = 0; i<menuItems.length; i++) {
-//                        contextMenu.add(getAdapterPosition(), i, i, menuItems[i]); //groupId, itemId, order, title
-//                    }
-//                }
-//            });
+            itemView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+                @Override
+                public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                    //Log.d("Position", "You long clicked on position "+getAdapterPosition());
+                    //Log.d("OnLongClick", "You long clicked on "+adapter.getItem(getAdapterPosition()).getInquiryName());
+                    //for (int i = 0; i<menuItems.length; i++) {
+                        contextMenu.add(0, getAdapterPosition(), 0, menuItems[0]); //groupId, itemId, order, title
+                    //}
+                }
+            });
         }
     }
 }
